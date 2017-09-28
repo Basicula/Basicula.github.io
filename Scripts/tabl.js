1e38c3377c4de3990510752ec0bedad54a30cd6f
Basicula <32364935+Basicula@users.noreply.github.com>
2017-09-28 20:22:20 +0300

    Update tabl.js

--- Scripts/tabl.js
+++ Scripts/tabl.js
@@ -72,7 +72,6 @@ function variablesInd() {
                 input.placeholder = "0";
                 input.type = "number";
                 input.required = "on";
-                input.value = 2;
                 td.appendChild(input);
                 tr.appendChild(td);
             }
